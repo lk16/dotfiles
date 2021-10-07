fa665aeafde3f76fc669951da408613623fe4e1b
Luuk Verweij <luuk.verweij@gig.com>
2021-10-07 17:34:59 +0200

    refactor: rename list_all_todo.sh -> grep_all_todo.sh

--- tools/grep_all_todo.sh
+++ tools/grep_all_todo.sh
@@ -7,7 +7,7 @@ dumpfile=$(mktemp)
 for folder in $(find ~ -maxdepth 1 -type d -name gig -o -name projects); do
     for subfolder in $(find $folder -maxdepth 1 -type d); do
         if [ -d "$subfolder/.git" ]; then
-            git --no-pager -C $subfolder grep -l 'TODO' | \
+            git --no-pager -C $subfolder grep $@ 'TODO' | \
                 sed "s,^,$subfolder/," | \
                 egrep -v 'notes/work/daily/202(0|1/0)' >> $dumpfile
         fi
