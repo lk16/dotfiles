459276618122e2bc2f46dbc95814b99efef9eb71
Luuk Verweij <luuk_verweij@msn.com>
2021-11-07 13:45:25 +0100

    refactor: call git status once per repo

--- tools/global_git_status.sh
+++ tools/global_git_status.sh
@@ -3,20 +3,22 @@
 # TODO make this a python script
 # TODO use -maxdepth 4 to cover ~/projects/INACTIEF
 
-# TODO make sure every branch is pushed
-
 GIT_REPOS=$(find ~ -maxdepth 3 -name .git -type d -prune -exec dirname {} \;)
 
 dirty_repos=0
 repos=0
 
 for repo in $GIT_REPOS; do
-    if [[ ! -z $(git -C $repo status -s) ]]; then
+    status=$(git -C $repo status -s)
+    if [[ ! -z $status ]]; then
         branch_name=$(git -C $repo rev-parse --abbrev-ref HEAD)
         echo -e "\033[1;31m$repo @ $branch_name\033[0m"
-        git -C $repo status -s
+        echo $status
         echo
         dirty_repos=$(expr $dirty_repos + 1)
+    # TODO git push dryrun is slow, consider caching somehow
+    #elif ! git push --all origin --dry-run; then
+    #    dirty_repos=$(expr $dirty_repos + 1)
     fi
     repos=$(expr $repos + 1)
 done
