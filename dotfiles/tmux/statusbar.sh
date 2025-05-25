d90b71f08dcc46c7ad9e7cb8cb455797a5c70a49
Luuk Verweij <luuk_verweij@msn.com>
2025-05-25 20:41:54 +0200

    make tmux statusbar script selfcontained

--- dotfiles/tmux/statusbar.sh
+++ dotfiles/tmux/statusbar.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+# Folder this script lives in.
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 source ~/.bashrc
-cd $DOTFILES_ROOT/tools
+cd $SCRIPT_DIR/../../tools
 source ./venv/bin/activate
 ./manage.py tmux-statusbar
