45a28110d611186c81c816f86c2630702bb41f79
Luuk Verweij <luuk_verweij@msn.com>
2025-05-30 19:31:26 +0200

    reorder bashrc, remove poetry from direnvrc

--- dotfiles/bash/rc.sh
+++ dotfiles/bash/rc.sh
@@ -18,109 +18,38 @@ case $- in
       *) return;;
 esac
 
-# add to PATH if they exist
+# --- Path ---
+
 [ -d $HOME/.local/bin ] && PATH=$PATH:$HOME/.local/bin
 [ -d /opt/bin ] && PATH=$PATH:/opt/bin
 [ -d /usr/local/bin ] && PATH=$PATH:/usr/local/bin
 
+# --- Rust ---
+
 [ -d $HOME/.cargo/bin ] && PATH=$PATH:$HOME/.cargo/bin
+
+# --- Go ---
 
 [ -d /usr/local/go/bin ] && PATH=$PATH:/usr/local/go/bin
 
+mkdir -p $HOME/.local/bin $HOME/.local/go
+export GOBIN=$HOME/.local/bin
+export GOPATH=$HOME/.local/go
+
+# --- Python ---
+
 [ -f $HOME/.rye/env ] && source $HOME/.rye/env
 
-# Set GOBIN to $HOME/.local/bin if it exists, Go will use this to install binaries
-[ -d $HOME/.local/bin ] && export GOBIN=$HOME/.local/bin
+# force colored output of pytest
+export PYTEST_ADDOPTS="--color=yes"
 
+# force colored output of mypy
+export MYPY_FORCE_COLOR=1
 
-# remove duplicates from PATH
-PATH=$(/usr/bin/python -c '
-import os
+# disable unreadable Typer stacktrace
+export _TYPER_STANDARD_TRACEBACK=1
 
-from collections import OrderedDict
-
-path = os.environ["PATH"].split(":")
-print(":".join(OrderedDict.fromkeys(path)))
-')
-
-# --- Aliases ---
-
-# TODO clean the aliases below
-
-# enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-    alias cg='grep --color=always'
-fi
-
-alias ccat='pygmentize -g'
-alias lcat='pygmentize -g -O style=colorful,linenos=1'
-
-# cut and paste
-alias c='xclip -in -selection clipboard'
-alias p='xclip -out -selection clipboard'
-
-alias ll='ls -l'
-alias la="ls -la"
-alias l='ls -CF'
-
-
-alias gig='git'
-
-alias gg="git log --graph --oneline --decorate --all"
-alias ggs="git status"
-alias gds="git diff --stat"
-alias ggsi="git status --ignored"
-alias ggc="git checkout"
-alias ggr="git rebase"
-alias ggrc="git rebase --continue"
-alias gitroot='git rev-parse --show-toplevel 2>/dev/null'
-alias gv="git rev-parse HEAD | cut -c -8"
-alias ggpf="git push --force-with-lease"
-alias gb="git branch --show-current"
-
-alias gggs='tools global-git-status'
-alias ggtop='git shortlog --summary --email | sort -rn'
-
-# allows using "git conflicts" to list conflicting files
-git config --global alias.conflicts "diff --name-only --diff-filter=U"
-
-alias json="python3 -m json.tool"
-
-alias dps="docker-compose ps"
-
-alias alert="mplayer $DOTFILES_ROOT/audio/airhorn.mp3 -loop 0 > /dev/null 2>&1"
-
-alias dc="docker compose"
-
-alias killdocker='docker ps -q | xargs docker stop'
-
-alias plock='poetry lock --no-update'
-
-alias pir='poetry install --sync'
-
-# --- Functions ---
-
-function gp() {
-    local branch=$(git branch --show-current)
-
-    if git branch -a | grep "origin/$branch" >/dev/null; then
-        git push $@
-    else
-        git push -u origin $branch $@
-    fi
-}
-
-function noise() {
-    pkill -f 'mplayer.*noise.ogg' || \
-    (mplayer $DOTFILES_ROOT/audio/noise.ogg -loop 0 > /dev/null 2>&1 &)
-}
-
-# --- Node Version Manager ---
+# --- Javascript ---
 
 if [ -d "$HOME/.nvm" ]; then
     export NVM_DIR="$HOME/.nvm"
@@ -128,77 +57,29 @@ if [ -d "$HOME/.nvm" ]; then
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
 
-# --- Shell options ---
+# --- Prompt and shell options ---
 
 # don't put duplicate lines in the history.
-# See bash(1) for more options
 HISTCONTROL=ignoredups
 
 # append to the history file, don't overwrite it
 shopt -s histappend
 
-# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+# don't limit history size
 HISTSIZE=-1
 HISTFILESIZE=-1
 
-# check the window size after each command and, if necessary,
-# update the values of LINES and COLUMNS.
+# update the values of LINES and COLUMNS after each command
 shopt -s checkwinsize
 
-# If set, the pattern "**" used in a pathname expansion context will
-# match all files and zero or more directories and subdirectories.
+# match all files and zero or more directories and subdirectories
 shopt -s globstar
-
-# make less more friendly for non-text input files, see lesspipe(1)
-[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
-
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
-
-
-# enable programmable completion features (you don't need to enable
-# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
-# sources /etc/bash.bashrc).
-if ! shopt -oq posix; then
-    if [ -f /usr/share/bash-completion/bash_completion ]; then
-        . /usr/share/bash-completion/bash_completion
-    elif [ -f /etc/bash_completion ]; then
-        . /etc/bash_completion
-    fi
-fi
 
 # enables: ctrl + backspace to delete a word
 stty werase \^H
 
-# --- Prompt ---
-
 export CLICOLOR=1
 export LSCOLORS=ExFxCxDxBxegedabagacad
-
-export COLOR_NO_COLOR='\e[0m'
-export COLOR_WHITE='\e[1;37m'
-export COLOR_BLACK='\e[0;30m'
-export COLOR_BLUE='\e[0;34m'
-export COLOR_LIGHT_BLUE='\e[1;34m'
-export COLOR_GREEN='\e[0;32m'
-export COLOR_LIGHT_GREEN='\e[1;32m'
-export COLOR_CYAN='\e[0;36m'
-export COLOR_LIGHT_CYAN='\e[1;36m'
-export COLOR_RED='\e[0;31m'
-export COLOR_LIGHT_RED='\e[1;31m'
-export COLOR_PURPLE='\e[0;35m'
-export COLOR_LIGHT_PURPLE='\e[1;35m'
-export COLOR_BROWN='\e[0;33m'
-export COLOR_YELLOW='\e[1;33m'
-export COLOR_GRAY='\e[0;30m'
-export COLOR_LIGHT_GRAY='\e[0;37m'
-
-
-# default prompt colors
-USER_PROMPT_COLOR=$COLOR_YELLOW
-USER_COMMAND_COLOR=$COLOR_LIGHT_PURPLE
 
 # Add git branch if its present to PS1
 parse_git_branch() {
@@ -216,30 +97,43 @@ parse_git_branch() {
 
 PROMPT_COMMAND=__prompt_command
 
-# With help from https://stackoverflow.com/a/16715681
-
-# Fixes from https://superuser.com/a/695350
-# Make your \[ and \] strictly matching non-nesting pairs.
-# Make sure that all non-printing sequences are within \[ and \] (and that, conversely, that all printing sequences are not).
-
 __prompt_command() {
     # has to be first
     local EXIT="$?"
 
-    # Add unwritten history item to ~/.bash_history
-    # Since this runs after every command completes, it should add every command to the history.
-    history -a
+    COLOR_NO_COLOR='\e[0m'
+    COLOR_WHITE='\e[1;37m'
+    COLOR_BLACK='\e[0;30m'
+    COLOR_BLUE='\e[0;34m'
+    COLOR_LIGHT_BLUE='\e[1;34m'
+    COLOR_GREEN='\e[0;32m'
+    COLOR_LIGHT_GREEN='\e[1;32m'
+    COLOR_CYAN='\e[0;36m'
+    COLOR_LIGHT_CYAN='\e[1;36m'
+    COLOR_RED='\e[0;31m'
+    COLOR_LIGHT_RED='\e[1;31m'
+    COLOR_PURPLE='\e[0;35m'
+    COLOR_LIGHT_PURPLE='\e[1;35m'
+    COLOR_BROWN='\e[0;33m'
+    COLOR_YELLOW='\e[1;33m'
+    COLOR_GRAY='\e[0;30m'
+    COLOR_LIGHT_GRAY='\e[0;37m'
 
-    # clear history
-    history -c
+    # default prompt colors
+    USER_PROMPT_COLOR=$COLOR_YELLOW
+    USER_COMMAND_COLOR=$COLOR_LIGHT_PURPLE
+
+    # add unwritten history item to ~/.bash_history
+    history -a
 
     # reload history from ~/.bash_history
     history -r
 
     PS1="\[${USER_PROMPT_COLOR}\]\W \[${COLOR_LIGHT_GREEN}\]\$(parse_git_branch)\[${USER_PROMPT_COLOR}\]"
 
+    # show dollar sign in red if last command failed
     if [ $EXIT != 0 ]; then
-        PS1+="\[${COLOR_LIGHT_RED}\]\$\[${USER_COMMAND_COLOR}\]"        # Add red if exit code non 0
+        PS1+="\[${COLOR_LIGHT_RED}\]\$\[${USER_COMMAND_COLOR}\]"
     else
         PS1+="\[${COLOR_LIGHT_GREEN}\]\$\[${USER_COMMAND_COLOR}\]"
     fi
@@ -247,16 +141,70 @@ __prompt_command() {
     PS1+=" \[${USER_COMMAND_COLOR}\]\[${COLOR_NO_COLOR}\]"
 }
 
-# --- Python tools ---
+# --- Aliases ---
 
-# force colored output of pytest
-export PYTEST_ADDOPTS="--color=yes"
+# TODO clean the aliases below
 
-# force colored output of mypy
-export MYPY_FORCE_COLOR=1
+# colorize ls, grep, etc.
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
 
-# disable unreadable Typer stacktrace
-export _TYPER_STANDARD_TRACEBACK=1
+# copy to clipboard
+alias c='xclip -in -selection clipboard'
+
+# common typo
+alias gig='git'
+
+# show git graph
+alias gg="git log --graph --oneline --decorate --all"
+
+# git status shortcut
+alias ggs="git status"
+
+# rebase continue shortcut
+alias ggrc="git rebase --continue"
+
+# output folder of repo root
+alias gitroot='git rev-parse --show-toplevel 2>/dev/null'
+
+# push force with lease
+alias ggpf="git push --force-with-lease"
+
+# show current branch
+alias gb="git branch --show-current"
+
+# global git status
+alias gggs='tools global-git-status'
+
+# play airhorn sound
+alias alert="mplayer $DOTFILES_ROOT/audio/airhorn.mp3 -loop 0 > /dev/null 2>&1"
+
+# docker compose shortcut
+alias dc="docker compose"
+
+# kill all docker containers
+alias killdocker='docker ps -q | xargs docker stop'
+
+# --- Functions ---
+
+# git push, create upstream branch if it doesn't exist
+function gp() {
+    local branch=$(git branch --show-current)
+
+    if git branch -a | grep "origin/$branch" >/dev/null; then
+        git push $@
+    else
+        git push -u origin $branch $@
+    fi
+}
+
+# Toggle white noise on loop, usually bound to hotkey `super` + `space`
+function noise() {
+    pkill -f 'mplayer.*noise.ogg' || \
+    (mplayer $DOTFILES_ROOT/audio/noise.ogg -loop 0 > /dev/null 2>&1 &)
+}
 
 # --- SSH config ---
 
@@ -265,6 +213,8 @@ export _TYPER_STANDARD_TRACEBACK=1
 
 # add work key if present
 [ -f $HOME/.ssh/id_rsa_work ] && eval $(keychain --eval id_rsa_work -q)
+
+# --- Shell hooks ---
 
 # load direnv if installed
 if which direnv > /dev/null; then
--- dotfiles/direnv/rc.sh
+++ dotfiles/direnv/rc.sh
@@ -1,22 +1,3 @@
-# For details see howto/direnv.md
-
-# Credit to https://rgoswami.me/posts/poetry-direnv/
-layout_poetry() {
-    if [[ ! -f pyproject.toml ]]; then
-        log_error 'No pyproject.toml found.  Use `poetry new` or `poetry init` to create one first.'
-        exit 2
-    fi
-
-    local VENV=$(dirname $(poetry run which python) 2>/dev/null)
-    export VIRTUAL_ENV=$(echo "$VENV" | rev | cut -d'/' -f2- | rev)
-    export POETRY_ACTIVE=1
-    PATH_add "$VENV"
-
-    if ! pip list 2>&1 | grep pdbpp > /dev/null; then
-        echo -e "\033[1;31mCould not find pdbpp!\033[0m"
-    fi
-}
-
 # Adapted from https://github.com/direnv/direnv/pull/1019/files
 layout_pdm() {
     PYPROJECT_TOML="${PYPROJECT_TOML:-pyproject.toml}"
